0f8daa2ffc8500849ec4205c7090ef55f5c8c66c
linsiting <zaq0809449@gmail.com>
2023-12-22 17:11:01 +0800

    正式

--- app.js
+++ app.js
@@ -1,85 +1,18 @@
 const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
-const sqlite3 = require('sqlite3');
-const { open } = require('sqlite');
-const { availableParallelism } = require('node:os');
-const cluster = require('node:cluster');
-const { createAdapter, setupPrimary } = require('@socket.io/cluster-adapter');
+const path = require('path');
 
-if (cluster.isPrimary) {
-  const numCPUs = availableParallelism();
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork({
-      PORT: 3000 + i
-    });
-  }
+const app = express();
 
-  return setupPrimary();
-}
+// Serve static files from the 'thirteen-branches' directory
+app.use(express.static(path.join(__dirname, 'thirteen-branches')));
 
-async function main() {
-  const db = await open({
-    filename: 'chat.db',
-    driver: sqlite3.Database
-  });
+// Send the main HTML file when the root route is accessed
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '13支-01157108+01157103.html'));
+  res.sendFile(path.join(__dirname, 'imgs'));
+});
 
-  await db.exec(`
-    CREATE TABLE IF NOT EXISTS messages (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      client_offset TEXT UNIQUE,
-      content TEXT
-    );
-  `);
-
-  const app = express();
-  const server = createServer(app);
-  const io = new Server(server, {
-    connectionStateRecovery: {},
-    adapter: createAdapter()
-  });
-
-  app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-  });
-
-  io.on('connection', async (socket) => {
-    socket.on('chat message', async (msg, clientOffset, callback) => {
-      let result;
-      try {
-        result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
-      } catch (e) {
-        if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
-          callback();
-        } else {
-          // nothing to do, just let the client retry
-        }
-        return;
-      }
-      io.emit('chat message', msg, result.lastID);
-      callback();
-    });
-
-    if (!socket.recovered) {
-      try {
-        await db.each('SELECT id, content FROM messages WHERE id > ?',
-          [socket.handshake.auth.serverOffset || 0],
-          (_err, row) => {
-            socket.emit('chat message', row.content, row.id);
-          }
-        )
-      } catch (e) {
-        // something went wrong
-      }
-    }
-  });
-
-  const port = process.env.PORT;
-
-  server.listen(port, () => {
-    console.log(`server running at http://localhost:${port}`);
-  });
-}
-
-main();
+// Start the server
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
